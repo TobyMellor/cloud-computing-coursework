db22dc8e0c9c3a46fef6acff541577fd3743c009
TobyMellor <tobymulberry@hotmail.com>
2018-10-31 18:30:14 +0000

    feat: more mocks, add some edge cases

--- src/services/anagram-service.ts
+++ src/services/anagram-service.ts
@@ -3,9 +3,17 @@ import config from '../config';
 export type AnagramSet = Set<string>;
 
 export function getNormalizedWord(word: string) {
-  const characters: string[] = word.split('');
-  const normalizedCharacters: string[] = characters.sort();
-  const normalizedWord: string = normalizedCharacters.join('');
+
+  // Convert to lower case, remove special characters
+  // e.g. E-mail becomes email
+  const cleanedWord = word.toLocaleLowerCase()
+                          .replace(/[^A-Za-z]/g, '');
+
+  // Normalize the word, so it's characters are sorted alphabetically
+  // e.g. email becomes aeilm
+  const normalizedWord = cleanedWord.split('')
+                                    .sort()
+                                    .join('');
 
   return normalizedWord;
 }
