c4217edd739999261cd065dd17e6ef602874236b
TobyMellor <tobymulberry@hotmail.com>
2018-11-12 18:19:15 +0000

    fix: use bash
    fix: check for 4 arguments
    
    fix: use which over whereis

--- map-reduce.sh
+++ map-reduce.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
+if [[ $4 -eq 0 ]] ; then
     echo 'Incorrect usage. Please specify the cluster ID, cluster region, input and output.'
     echo 'Example: ./map-reduce.sh cluster-4751 global gs://coc105-coursework/10.txt gs://coc105-coursework-output/test-output'
     exit 1
@@ -14,14 +14,14 @@ base_script="#!/bin/sh
 tar -xzf package.tar.gz
 
 # Execute script with the included node binary
-./$(whereis node)"
+./$(which node)"
 mapper_script="$base_script dist/mapper.js"
 reducer_script="$base_script dist/reducer.js"
 
 echo "$mapper_script" > ./dist/mapper_init.sh
 echo "$reducer_script" > ./dist/reducer_init.sh
 
-tar -cvf package.tar.gz dist \"$(whereis node)\"
+tar -cvf package.tar.gz dist \"$(which node)\"
 
 gcloud dataproc jobs submit hadoop \
   --cluster $1 \
