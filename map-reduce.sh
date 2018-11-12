551f487b50dc44d09742aea313e4504655087c7c
TobyMellor <tobymulberry@hotmail.com>
2018-11-12 18:13:28 +0000

    feat: ability to change cluster and region through script

--- map-reduce.sh
+++ map-reduce.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Incorrect usage. Please specify the input and output.'
-    echo 'Example: ./map-reduce.sh gs://coc105-coursework/10.txt gs://coc105-coursework-output/test-output'
+    echo 'Incorrect usage. Please specify the cluster ID, cluster region, input and output.'
+    echo 'Example: ./map-reduce.sh cluster-4751 global gs://coc105-coursework/10.txt gs://coc105-coursework-output/test-output'
     exit 1
 fi
 
@@ -24,11 +24,11 @@ echo "$reducer_script" > ./dist/reducer_init.sh
 tar -cvf package.tar.gz dist \"$(whereis node)\"
 
 gcloud dataproc jobs submit hadoop \
-  --cluster cluster-4751 \
-  --region=global \
+  --cluster $1 \
+  --region=$2 \
   --jar file:///usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
   --files package.tar.gz,./dist/mapper_init.sh,./dist/reducer_init.sh \
   -- -mapper 'sh mapper_init.sh' \
      -reducer 'sh reducer_init.sh' \
-     -input $1 \
-     -output $2
+     -input $3 \
+     -output $4
